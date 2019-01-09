32bb4f193f2156219745bcb68f3e9afede9453f6
yatoooon <qazasdeszplm@126.com>
2019-01-09 23:50:30 +0800

    整理一下线程

--- thread/src/main/java/com/yatoooon/thread/MainActivity.java
+++ thread/src/main/java/com/yatoooon/thread/MainActivity.java
@@ -13,11 +13,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setExample1();   //最简单的Thread
-        setExample2();   //最简单的Runnable
-        setExample3();   //ThreadFactory
-        setExample4();   //Executor 线程池
+//        setExample1();   //最简单的Thread
+//        setExample2();   //最简单的Runnable
+//        setExample3();   //ThreadFactory
+//        setExample4();   //Executor 线程池
+//        setExample5();   //Callable Future
+//        setExample6();     //引入线程同步  和  monitor
 
+    }
+
+
+    private Object monitor1 = new Object();
+    private Object monitor2 = new Object();
+
+    private int x = 0;
+    private int y = 0;
+    private int z = 0;
+
+    private void count2(int newValue) {  //线程同步第三种简单写法添加两个monitor      这样可以让两个线程分别访问这两个方法
+        synchronized (monitor1) {
+            x = newValue;
+            y = newValue;
+            if (x != y) {
+                Timber.d("x: " + x + ", y:" + y);
+            }
+        }
+
+    }
+
+    private void countZ2() {
+        synchronized (monitor2) {
+            z++;
+        }
+    }
+
+    private synchronized void count(int newValue) {  //线程同步第一种简单写法     count和countZ都被一个线程占有了
+        x = newValue;
+        y = newValue;
+        if (x != y) {
+            Timber.d("x: " + x + ", y:" + y);
+        }
+    }
+
+    private void countZ() {
+        synchronized (this) {  //线程同步第二种简单写法
+            z++;
+        }
+    }
+
+    private void setExample6() {
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000_000; i++) {
+                    count(i);
+                    countZ();
+                }
+                Timber.d(String.valueOf("countZ" + z));
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000_000; i++) {
+                    count(i);
+                    countZ();
+                }
+                Timber.d(String.valueOf("countZ" + z));
+            }
+        }.start();
+    }
+
+    private void setExample5() {
+        Callable<String> callable = new Callable<String>() {   //带返回值
+            @Override
+            public String call() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "Callable End";
+            }
+        };
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> submit = executorService.submit(callable);
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (submit.isDone()) {
+                try {
+                    String result = submit.get(); //阻塞  需要判断是否isDone
+                    Timber.d(result);
+                    break;
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
