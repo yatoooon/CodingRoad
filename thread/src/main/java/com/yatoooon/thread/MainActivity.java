97cf3edd0b8703a388a3346ff0ce51a8249f38f6
yatoooon <qazasdeszplm@126.com>
2019-01-10 23:25:51 +0800

    整理一下线程

--- thread/src/main/java/com/yatoooon/thread/MainActivity.java
+++ thread/src/main/java/com/yatoooon/thread/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import timber.log.Timber;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,78 @@ public class MainActivity extends AppCompatActivity {
 //        setExample3();   //ThreadFactory
 //        setExample4();   //Executor 线程池
 //        setExample5();   //Callable Future
-//        setExample6();     //引入线程同步  和  monitor
+//        setExample6();   //引入线程同步  和  monitor
+//        setExample7();   //volatile   AtomicInteger...
+        setExample8();    //Lock
 
+
+    }
+
+
+    private int l = 0;
+
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    Lock readLock = readWriteLock.readLock();
+    Lock writeLock = readWriteLock.writeLock();
+
+    private void count() {
+        writeLock.lock();
+        try {
+            x++;
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    private void print(int time) {
+        readLock.lock();
+        try {
+            for (int i = 0; i < time; i++) {
+                Timber.d("print" + x);
+            }
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    Lock lock = new ReentrantLock();
+
+    private void setExample8() {
+        lock.lock();   //一般不会直接这么用  常用的是  ReadWriteLock
+        try {
+            x++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    volatile int v = 0;
+    volatile AtomicInteger A = new AtomicInteger(0);
+
+    private void setExample7() {
+        //volatile保证可见性，不保证原子性，部分保证有序性（仅保证被volatile修饰的变量）。
+        //java.util.concurrent.atomic 下面的原子操作类 AtomicInteger等   保证原子性
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000_000; i++) {
+                    v++;
+                    A.getAndIncrement();
+                }
+                Timber.d(String.valueOf("countV" + v + "       countA" + A.get()));
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000_000; i++) {
+                    v++;
+                    A.getAndIncrement();
+                }
+                Timber.d(String.valueOf("countV" + v + "        countA" + A.get()));
+            }
+        }.start();
     }
 
 
@@ -62,6 +136,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setExample6() {
+        //synchronized 的本质:1.保证方法内部或者代码块内部资源(数据)的互斥访问.同一时间,由同一个monitor监视的代码,最多只能有一个线程在访问.
+        //                   2.保证线程之间对监视资源的数据同步.任何线程在获取到monitor后的第一时间,会先将共享内存中的数据复制到自己的缓存中;任何线程在释放monitor的第一时间,会先将缓存中的数据复制到共享内存中.
+
+
         new Thread() {
             @Override
             public void run() {
