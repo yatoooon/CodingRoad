f435b200175900b9a36c49f0182ba537078baa9c
yatoooon <qazasdeszplm@126.com>
2019-01-15 23:54:41 +0800

    整理一下自定义view

---
+++ baselibrary/src/main/java/com/yatoooon/baselibrary/utils/DensityUtil.java
@@ -0,0 +1,44 @@
+package com.yatoooon.baselibrary.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.WindowManager;
+
+public class DensityUtil {
+    public static int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
+    }
+
+    public static int sp2px(int sp) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, sp, Resources.getSystem().getDisplayMetrics());
+    }
+
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        return width;
+    }
+
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int height = dm.heightPixels;
+        return height;
+    }
+
+    public static int[] getScreenWidthHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        return new int[]{width, height};
+    }
+}
---
+++ customview/src/main/java/com/yatoooon/customview/draw/DashBoardView.java
@@ -0,0 +1,60 @@
+package com.yatoooon.customview.draw;
+
+import android.content.Context;
+import android.graphics.*;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import com.yatoooon.baselibrary.utils.DensityUtil;
+
+public class DashBoardView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final int RADIUS = DensityUtil.dp2px(100);
+    private static final int LENGTH = DensityUtil.dp2px(50);
+    private static final float ANGLE = 120;
+    PathDashPathEffect dashPathEffect;
+
+    Path path = new Path();
+
+    public DashBoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    {
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.STROKE);
+        path.addRect(0, 0, DensityUtil.dp2px(2), DensityUtil.dp2px(10), Path.Direction.CW);
+
+        Path arc = new Path();
+        arc.addArc(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS, ANGLE / 2 + 90, 360 - ANGLE);
+        PathMeasure pathMeasure = new PathMeasure(arc, false);
+
+        dashPathEffect = new PathDashPathEffect(path, (pathMeasure.getLength() - DensityUtil.dp2px(2)) / 10, 0, PathDashPathEffect.Style.ROTATE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawArc(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS, ANGLE / 2 + 90, 360 - ANGLE, false, paint);
+
+        paint.setPathEffect(dashPathEffect);
+        canvas.drawArc(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS, ANGLE / 2 + 90, 360 - ANGLE, false, paint);
+        paint.setPathEffect(null);
+
+        canvas.drawLine(
+                getWidth() / 2,
+                getHeight() / 2,
+                (float) Math.cos(Math.toRadians(calculatePoint(5))) * LENGTH + getWidth() / 2,
+                (float) Math.sin(Math.toRadians(calculatePoint(5))) * LENGTH + getHeight() / 2,
+                paint);
+
+    }
+
+    private int calculatePoint(int point) {
+        return (int) ((360 - ANGLE) / 10 * point + 150);
+    }
+
+
+}
