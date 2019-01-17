a375fd48a1d20d3acbc60f7930e15e90ade70047
yatoooon <qazasdeszplm@126.com>
2019-01-17 23:52:55 +0800

    整理一下自定义view

---
+++ customview/src/main/java/com/yatoooon/customview/draw/MixImageTextView.java
@@ -0,0 +1,39 @@
+package com.yatoooon.customview.draw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import com.yatoooon.baselibrary.utils.DensityUtil;
+
+import static android.graphics.Paint.ANTI_ALIAS_FLAG;
+
+public class MixImageTextView extends View {
+
+
+    TextPaint paint = new TextPaint(ANTI_ALIAS_FLAG);
+    String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean justo sem, sollicitudin in maximus a, vulputate id magna. Nulla non quam a massa sollicitudin commodo fermentum et est. Suspendisse potenti. Praesent dolor dui, dignissim quis tellus tincidunt, porttitor vulputate nisl. Aenean tempus lobortis finibus. Quisque nec nisl laoreet, placerat metus sit amet, consectetur est. Donec nec quam tortor. Aenean aliquet dui in enim venenatis, sed luctus ipsum maximus. Nam feugiat nisi rhoncus lacus facilisis pellentesque nec vitae lorem. Donec et risus eu ligula dapibus lobortis vel vulputate turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In porttitor, risus aliquam rutrum finibus, ex mi ultricies arcu, quis ornare lectus tortor nec metus. Donec ultricies metus at magna cursus congue. Nam eu sem eget enim pretium venenatis. Duis nibh ligula, lacinia ac nisi vestibulum, vulputate lacinia tortor.";
+
+    public MixImageTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    {
+        paint.setTextSize(DensityUtil.dp2px(20));
+        paint.setColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+        //只绘制文字的话用staticlayout 就行
+//        StaticLayout.Builder.obtain(text, 0, text.length(), paint, getWidth()).build().draw(canvas);
+
+        
+    }
+}
